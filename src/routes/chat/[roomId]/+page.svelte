f04838fee71edcf5666f64bc91cfbae2cf0070c7
haker <haker@pop-os.localdomain>
2024-03-18 20:30:58 +0400

    key name fix

--- src/routes/chat/[roomId]/+page.svelte
+++ src/routes/chat/[roomId]/+page.svelte
@@ -12,20 +12,33 @@
     let userId;
     let roomId;
     let isInit = false;
+    let typingUsersSet = new Set();
+    let typingUsers=[];
+
+    let localuserId;
 
     onMount(() => {
-        console.log("$page.params =>",$page.params)
+        // console.log("$page.params =>",$page.params)
         roomId = $page.params.roomId;
-        console.log("roomId =>",roomId)
+        // console.log("roomId =>",roomId)
 
-        userId = $page.url.searchParams.get('userId');
+        let userId = $page.url.searchParams.get('userId');
 
         function toUser(user){
             return String(user).split("//")[0];
         }
 
+        // for (let i=0;i < 200; i++){
+
+        // localuserId=`${userId}_${i}`
+        localuserId=userId
+
+        // console.log(`started ${i}`)
+
         // Replace 'ws://localhost:4000/socket' with your actual WebSocket endpoint
-        socket = new Socket('ws://localhost:4000/socket',{params: {user_id: userId, room_id: roomId}});
+        // socket = new Socket('wss://chat-app-dev.fly.dev/socket',{params: {user_id: userId, room_id: roomId}});
+        socket = new Socket('ws://localhost:4000/socket',{params: {user_id: localuserId, room_id: roomId}});
+
         socket.connect();
 
         // Join the channel
@@ -40,19 +53,30 @@
 
         // Listen for incoming messages
         channel.on('new_msg', (payload) => {
-            console.log("payload=>",payload)
-            messages = [...messages, { user: toUser(payload.user), text: payload.text }];
+            // console.log("payload=>",payload)
+            messages = [...messages, { user: toUser(payload.username), text: payload.text }];
         });
 
         // Listen for presence state and diffs
         channel.on("presence_state", state => {
             if (!isInit) {
+                isInit = true;
+
+                // typingUsers = Object.keys(state).filter(usr => {console.log(state[usr].metas[0].isTyping);
+                // return state[usr].metas[0].isTyping == true;})
+
+                // let typingUsersUniq = users.map(usr=>usr.split("//")[0])
+                // typingUsersSet = new Set(typingUsersUniq);
+
+
+                // console.log("typingUsers=>",typingUsers);
+                // console.log("state=>",state);
+
                 let allUsers = Object.keys(state)
                 users=[...new Set([...users, ...allUsers])];
                 let usersUniq = users.map(usr=>usr.split("//")[0])
                 userSet = new Set(usersUniq);
-                isInit = true;
-                console.log("presence state:", state);
+                // console.log("presence state:", state);
             }
         });
 
@@ -60,20 +84,35 @@
             if(isInit) {
                 if (Object.keys(diff.leaves).length > 0) {
                     let usersToRemove = Object.keys(diff.leaves)
-                    console.log("usersToRemove =>", usersToRemove)
+                    // console.log("usersToRemove =>", usersToRemove)
                     users = users.filter(user => !usersToRemove.includes(user));
                     let usersUniq = users.map(usr=>usr.split("//")[0])
                     userSet=new Set(usersUniq);
+
+                    // //typing users
+                    // let typingusersToRemove = Object.keys(diff.leaves).filter(usr => diff.leaves[usr].metas[0].isTyping == false)
+                    // console.log("typingusersToRemove =>", typingusersToRemove)
+                    // typingUsers = typingUsers.filter(user => !typingusersToRemove.includes(user));
+                    // let typingusersUniq = typingUsers.map(usr=>usr.split("//")[0])
+                    // typingUsersSet=new Set(typingusersUniq);
                 }
                 if (Object.keys(diff.joins).length > 0) {
                     let newUsers = Object.keys(diff.joins)
                     users = [...new Set([...users, ...newUsers])];
                     let usersUniq = users.map(usr=>usr.split("//")[0])
                     userSet=new Set(usersUniq);
+
+                    // //typing users
+                    // let newtypingUsers = Object.keys(diff.joins).filter(usr => diff.joins[usr].metas[0].isTyping == true)
+                    // typingUsers = [...new Set([...typingUsers, ...newtypingUsers])];
+                    // let typingusersUniq = typingUsers.map(usr=>usr.split("//")[0])
+                    // typingUsersSet=new Set(typingusersUniq);
                 }
-                console.log("presence diff:", diff);
+
             }
-        });
+
+       });
+    // }
 
 
     });
@@ -84,6 +123,11 @@
             channel.push('new_msg', { chatmsg: message });
             message = '';
         }
+    }
+
+    function changeMessage() {
+        console.log("change message pushing");
+        channel.push('change_msg', { chatmsg: message });
     }
 
 </script>
@@ -98,7 +142,15 @@
         {/each}
     </ul>
 
-<br>
+    <h2>Typing users</h2>
+    <ul>
+        {#each typingUsersSet as user}
+            <li>{user}</li>
+        {/each}
+    </ul>
+
+    <br>
+    
     <input
             type="text"
             placeholder="Type a message..."
